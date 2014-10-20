bdb2f2c084680ab0aa18a13af27d9926d2cb78b3
Roland Huß <roland@jolokia.org>
2014-10-20 12:24:50 +0200

    Initial commit

---
+++ src/test/java/org/jolokia/docker/maven/sample/jolokia/LogServiceIT.java
@@ -0,0 +1,45 @@
+package org.jolokia.docker.maven.sample.jolokia;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.parsing.Parser;
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * @author roland
+ * @since 15.05.14
+ */
+public class LogServiceIT {
+
+    @Test
+    public void testLog() {
+
+        String baseUrl = extractUrl();
+        long nonce = (int) (Math.random() * 10000);
+        System.out.println("Checking URL: " + baseUrl);
+
+        RestAssured.baseURI = baseUrl;
+        RestAssured.defaultParser = Parser.TEXT;
+        // Need to do it that way since Jolokia doesnt return application/json as mimetype by default
+        String answer = get("/" + nonce).asString();
+        System.out.println("Response: " + answer);
+
+        given()
+                .param("mimeType", "application/json")
+                .get("/" + nonce)
+        .then().assertThat()
+                .header("content-type", containsString("text/plain"))
+                .body(containsString(nonce + ""));
+    }
+
+    private String extractUrl() {
+        String ret = System.getProperty("log.url");
+        if (ret == null) {
+            throw new IllegalArgumentException("Cannot extract service url as system property");
+        }
+        return ret;
+    }
+
+}
